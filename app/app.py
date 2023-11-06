1930c1273ff4b0d216ea9b521219bbb141d99957
yixliu1 <yixliu1@student.unimelb.edu.au>
2023-11-06 21:40:27 +0800

    app

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI
 from main import query
 from env import chain
 from pydantic import BaseModel
+from result_process import process_result
+import re
 
 
 app = FastAPI()
@@ -14,6 +16,14 @@ class Input(BaseModel):
 @app.post("/task1")
 async def process_resume(input: Input):
     # Simulate processing the resume string
-    # print(query(input.personal_information))
-    res = chain({"query": query(input.personal_information)})
-    return res['result']
+    for i in range(3):
+        res = chain({"query": query(input.personal_information)})
+        chunks = re.split(r'(\d+\.\s*Occupation:)', res['result'], flags=re.IGNORECASE)
+        if len(chunks) > 1:
+            break
+    if len(chunks) > 1:
+        output = process_result(chunks)
+    else:
+        output = res['result']
+
+    return output
