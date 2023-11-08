bfe620b631ba03ae1febe3d6c447ae4104dc3c68
yixliu1 <yixliu1@student.unimelb.edu.au>
2023-11-08 13:49:35 +0800

    embedding

--- app/qna/llm.py
+++ app/qna/llm.py
@@ -6,11 +6,11 @@ from langchain.chat_models import ChatOpenAI
 from langchain.llms.base import LLM
 from langchain.embeddings.base import Embeddings
 from langchain.embeddings import OpenAIEmbeddings
-
 from qna.constants import (
     OPENAI_COMPLETIONS_ENGINE,
     OPENAI_EMBEDDINGS_ENGINE,
 )
+
 
 if TYPE_CHECKING:
     from langchain.vectorstores.redis import Redis as RedisVDB
@@ -21,7 +21,7 @@ def get_llm(max_tokens=100) -> LLM:
     return llm
 
 
-def get_embeddings() -> Embeddings:
+def get_embeddings():
     embeddings = OpenAIEmbeddings(model_name=OPENAI_EMBEDDINGS_ENGINE)
     return embeddings
 
